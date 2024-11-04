43166ee57d49546c4755dba76ee47524e1489d8d
Dzianis <denispokhodnia@gmail.com>
2024-11-04 07:28:28 +0300

    add files

---
+++ LaboratoryWork10/Task1New/calculation.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATION_H
+#define CALCULATION_H
+
+#include <string>
+
+void calculate_and_write(double a, double b, double c, double x_start, double x_end, double x_step, const std::string& filename);
+
+#endif // CALCULATION_H
+
---
+++ LaboratoryWork10/Task1New/quadratic.cpp
@@ -0,0 +1,6 @@
+#include "quadratic.h"
+
+double quadratic(double a, double b, double c, double x) {
+    return a * x * x + b * x + c;
+}
+
